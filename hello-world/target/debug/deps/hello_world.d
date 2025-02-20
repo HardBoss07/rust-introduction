e6a9eaeaccaa859a53b86b10b669dc9c7dc3e516
HardBoss07 <matteo@bosshard.net>
2025-02-20 11:32:51 +0100

    init commit with hello world program

---
+++ hello-world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Code Projects\rust-introduction\hello-world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Code Projects\rust-introduction\hello-world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+C:\Code\ Projects\rust-introduction\hello-world\target\debug\hello-world.exe: C:\Code\ Projects\rust-introduction\hello-world\src\main.rs
